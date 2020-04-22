83573e29de8b5c96b92b75d2169bd3d272663001
Theo Vora <theo.vora@gmail.com>
2020-04-22 15:33:11 -0600

    Display images in chat

--- ios-starter/swift-starter/FriendlyChatSwift/FCViewController.swift
+++ ios-starter/swift-starter/FriendlyChatSwift/FCViewController.swift
@@ -120,11 +120,32 @@ UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDele
         let messageSnapshot = self.messages[indexPath.row]
         guard let message = messageSnapshot.value as? [String: String] else { return cell }
         let name = message[Constants.MessageFields.name] ?? ""
-        let text = message[Constants.MessageFields.text] ?? ""
-        cell.textLabel?.text = name + ": " + text
-        cell.imageView?.image = UIImage(named: "ic_account_circle")
-        if let photoURL = message[Constants.MessageFields.photoURL], let url = URL(string: photoURL), let data = try? Data(contentsOf: url) {
-            cell.imageView?.image = UIImage(data: data)
+        
+        // if there is an image, display it.
+        if let imageURL = message[Constants.MessageFields.imageURL] {
+            if imageURL.hasPrefix("gs://") {
+                Storage.storage().reference(forURL: imageURL).getData(maxSize: INT64_MAX) {(data, error) in
+                    if let error = error {
+                        print("Error downloading: \(error)")
+                        return
+                    }
+                    DispatchQueue.main.async {
+                        cell.imageView?.image = UIImage.init(data: data!)
+                        cell.setNeedsLayout()
+                    }
+                }
+            } else if let URL = URL(string: imageURL), let data = try? Data(contentsOf: URL) {
+                cell.imageView?.image = UIImage.init(data: data)
+            }
+            cell.textLabel?.text = "sent by: \(name)"
+        } else {
+            // just display the text message
+            let text = message[Constants.MessageFields.text] ?? ""
+            cell.textLabel?.text = name + ": " + text
+            cell.imageView?.image = UIImage(named: "ic_account_circle")
+            if let photoURL = message[Constants.MessageFields.photoURL], let url = URL(string: photoURL), let data = try? Data(contentsOf: url) {
+                cell.imageView?.image = UIImage(data: data)
+            }
         }
         return cell
     }
