c9e8cd7ee879c4c69285f2251be81fc62668fa21
Theo Vora <theo.vora@gmail.com>
2020-04-21 20:05:07 -0600

    Display chat messages

--- ios-starter/swift-starter/FriendlyChatSwift/FCViewController.swift
+++ ios-starter/swift-starter/FriendlyChatSwift/FCViewController.swift
@@ -30,168 +30,188 @@ let kBannerAdUnitID = "ca-app-pub-3940256099942544/2934735716"
 
 @objc(FCViewController)
 class FCViewController: UIViewController, UITableViewDataSource, UITableViewDelegate,
-    UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
-  // Instance variables
-  @IBOutlet weak var textField: UITextField!
-  @IBOutlet weak var sendButton: UIButton!
-  var ref: DatabaseReference!
-  var messages: [DataSnapshot] = []
-  var msglength: NSNumber = 10
-  fileprivate var _refHandle: DatabaseHandle!
-
-  var storageRef: StorageReference!
-  var remoteConfig: RemoteConfig!
-
-  @IBOutlet weak var banner: GADBannerView!
-  @IBOutlet weak var clientTable: UITableView!
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-
-    self.clientTable.register(UITableViewCell.self, forCellReuseIdentifier: "tableViewCell")
-
-    configureDatabase()
-    configureStorage()
-    configureRemoteConfig()
-    fetchConfig()
-    loadAd()
-  }
-
-  deinit {
-  }
-
-  func configureDatabase() {
-  }
-
-  func configureStorage() {
-  }
-
-  func configureRemoteConfig() {
-  }
-
-  func fetchConfig() {
-  }
-
-  @IBAction func didPressFreshConfig(_ sender: AnyObject) {
-    fetchConfig()
-  }
-
-  @IBAction func didSendMessage(_ sender: UIButton) {
-    _ = textFieldShouldReturn(textField)
-  }
-
-  @IBAction func didPressCrash(_ sender: AnyObject) {
-  }
-
-  @IBAction func inviteTapped(_ sender: AnyObject) {
-  }
-
-  func loadAd() {
-  }
-
-  func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-    guard let text = textField.text else { return true }
-
-    let newLength = text.utf16.count + string.utf16.count - range.length
-    return newLength <= self.msglength.intValue // Bool
-  }
-
-  // UITableViewDataSource protocol methods
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return messages.count
-  }
-
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    // Dequeue cell
-    let cell = self.clientTable.dequeueReusableCell(withIdentifier: "tableViewCell", for: indexPath)
-
-    return cell
-  }
-
-  // UITextViewDelegate protocol methods
-  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-    guard let text = textField.text else { return true }
-    textField.text = ""
-    view.endEditing(true)
-    let data = [Constants.MessageFields.text: text]
-    sendMessage(withData: data)
-    return true
-  }
-
-  func sendMessage(withData data: [String: String]) {
-  }
-
-  // MARK: - Image Picker
-
-  @IBAction func didTapAddPhoto(_ sender: AnyObject) {
-    let picker = UIImagePickerController()
-    picker.delegate = self
-    if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
-      picker.sourceType = UIImagePickerController.SourceType.camera
-    } else {
-      picker.sourceType = UIImagePickerController.SourceType.photoLibrary
+UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // Instance variables
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
+    var ref: DatabaseReference!
+    var messages: [DataSnapshot] = []
+    var msglength: NSNumber = 10
+    fileprivate var _refHandle: DatabaseHandle!
+    
+    var storageRef: StorageReference!
+    var remoteConfig: RemoteConfig!
+    
+    @IBOutlet weak var banner: GADBannerView!
+    @IBOutlet weak var clientTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.clientTable.register(UITableViewCell.self, forCellReuseIdentifier: "tableViewCell")
+        
+        configureDatabase()
+        configureStorage()
+        configureRemoteConfig()
+        fetchConfig()
+        loadAd()
     }
-
-    present(picker, animated: true, completion:nil)
-  }
-
-  func imagePickerController(_ picker: UIImagePickerController,
-    didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-// Local variable inserted by Swift 4.2 migrator.
-let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
-
-      picker.dismiss(animated: true, completion:nil)
-    guard let uid = Auth.auth().currentUser?.uid else { return }
-
-    // if it's a photo from the library, not an image from the camera
-    if #available(iOS 8.0, *), let referenceURL = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.referenceURL)] as? URL {
-      let assets = PHAsset.fetchAssets(withALAssetURLs: [referenceURL], options: nil)
-      let asset = assets.firstObject
-      asset?.requestContentEditingInput(with: nil, completionHandler: { (contentEditingInput, info) in
-        let imageFile = contentEditingInput?.fullSizeImageURL
-        let filePath = "\(uid)/\(Int(Date.timeIntervalSinceReferenceDate * 1000))/\((referenceURL as AnyObject).lastPathComponent!)"
-      })
-    } else {
-      guard let image = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.originalImage)] as? UIImage else { return }
-      let imageData = image.jpegData(compressionQuality: 0.8)
-      guard let uid = Auth.auth().currentUser?.uid else { return }
-      let imagePath = "\(uid)/\(Int(Date.timeIntervalSinceReferenceDate * 1000)).jpg"
+    
+    deinit {
+        if let refHandle = _refHandle {
+            self.ref.child("messages").removeObserver(withHandle: refHandle)
+        }
     }
-  }
-
-  func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-    picker.dismiss(animated: true, completion:nil)
-  }
-
-  @IBAction func signOut(_ sender: UIButton) {
-    let firebaseAuth = Auth.auth()
-    do {
-        try firebaseAuth.signOut()
-        dismiss(animated: true, completion: nil)
-    } catch let signOutError as NSError {
-        print("Error signing out: \(signOutError.localizedDescription)")
+    
+    func configureDatabase() {
+        ref = Database.database().reference()
+        
+        // Listen for new messages in the Firebase database
+        _refHandle = self.ref.child("messages").observe(.childAdded, with: { [weak self] (snapshot) -> Void in
+            guard let strongSelf = self else { return }
+            strongSelf.messages.append(snapshot)
+            strongSelf.clientTable.insertRows(at: [IndexPath(row: strongSelf.messages.count-1, section: 0)], with: .automatic)
+        })
     }
-  }
-
-  func showAlert(withTitle title: String, message: String) {
-    DispatchQueue.main.async {
-        let alert = UIAlertController(title: title,
-            message: message, preferredStyle: .alert)
-        let dismissAction = UIAlertAction(title: "Dismiss", style: .destructive, handler: nil)
-        alert.addAction(dismissAction)
-        self.present(alert, animated: true, completion: nil)
+    
+    func configureStorage() {
     }
-  }
-
+    
+    func configureRemoteConfig() {
+    }
+    
+    func fetchConfig() {
+    }
+    
+    @IBAction func didPressFreshConfig(_ sender: AnyObject) {
+        fetchConfig()
+    }
+    
+    @IBAction func didSendMessage(_ sender: UIButton) {
+        _ = textFieldShouldReturn(textField)
+    }
+    
+    @IBAction func didPressCrash(_ sender: AnyObject) {
+    }
+    
+    @IBAction func inviteTapped(_ sender: AnyObject) {
+    }
+    
+    func loadAd() {
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let text = textField.text else { return true }
+        
+        let newLength = text.utf16.count + string.utf16.count - range.length
+        return newLength <= self.msglength.intValue // Bool
+    }
+    
+    // UITableViewDataSource protocol methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Dequeue cell
+        let cell = self.clientTable.dequeueReusableCell(withIdentifier: "tableViewCell", for: indexPath)
+        
+        let messageSnapshot = self.messages[indexPath.row]
+        guard let message = messageSnapshot.value as? [String: String] else { return cell }
+        let name = message[Constants.MessageFields.name] ?? ""
+        let text = message[Constants.MessageFields.text] ?? ""
+        cell.textLabel?.text = name + ": " + text
+        cell.imageView?.image = UIImage(named: "ic_account_circle")
+        if let photoURL = message[Constants.MessageFields.photoURL], let url = URL(string: photoURL), let data = try? Data(contentsOf: url) {
+            cell.imageView?.image = UIImage(data: data)
+        }
+        return cell
+    }
+    
+    // UITextViewDelegate protocol methods
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        guard let text = textField.text else { return true }
+        textField.text = ""
+        view.endEditing(true)
+        let data = [Constants.MessageFields.text: text]
+        sendMessage(withData: data)
+        return true
+    }
+    
+    func sendMessage(withData data: [String: String]) {
+    }
+    
+    // MARK: - Image Picker
+    
+    @IBAction func didTapAddPhoto(_ sender: AnyObject) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
+            picker.sourceType = UIImagePickerController.SourceType.camera
+        } else {
+            picker.sourceType = UIImagePickerController.SourceType.photoLibrary
+        }
+        
+        present(picker, animated: true, completion:nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // Local variable inserted by Swift 4.2 migrator.
+        let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
+        
+        picker.dismiss(animated: true, completion:nil)
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        // if it's a photo from the library, not an image from the camera
+        if #available(iOS 8.0, *), let referenceURL = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.referenceURL)] as? URL {
+            let assets = PHAsset.fetchAssets(withALAssetURLs: [referenceURL], options: nil)
+            let asset = assets.firstObject
+            asset?.requestContentEditingInput(with: nil, completionHandler: { (contentEditingInput, info) in
+                let imageFile = contentEditingInput?.fullSizeImageURL
+                let filePath = "\(uid)/\(Int(Date.timeIntervalSinceReferenceDate * 1000))/\((referenceURL as AnyObject).lastPathComponent!)"
+            })
+        } else {
+            guard let image = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.originalImage)] as? UIImage else { return }
+            let imageData = image.jpegData(compressionQuality: 0.8)
+            guard let uid = Auth.auth().currentUser?.uid else { return }
+            let imagePath = "\(uid)/\(Int(Date.timeIntervalSinceReferenceDate * 1000)).jpg"
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion:nil)
+    }
+    
+    @IBAction func signOut(_ sender: UIButton) {
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+            dismiss(animated: true, completion: nil)
+        } catch let signOutError as NSError {
+            print("Error signing out: \(signOutError.localizedDescription)")
+        }
+    }
+    
+    func showAlert(withTitle title: String, message: String) {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: title,
+                                          message: message, preferredStyle: .alert)
+            let dismissAction = UIAlertAction(title: "Dismiss", style: .destructive, handler: nil)
+            alert.addAction(dismissAction)
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
 }
 
 // Helper function inserted by Swift 4.2 migrator.
 fileprivate func convertFromUIImagePickerControllerInfoKeyDictionary(_ input: [UIImagePickerController.InfoKey: Any]) -> [String: Any] {
-	return Dictionary(uniqueKeysWithValues: input.map {key, value in (key.rawValue, value)})
+    return Dictionary(uniqueKeysWithValues: input.map {key, value in (key.rawValue, value)})
 }
 
 // Helper function inserted by Swift 4.2 migrator.
 fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
-	return input.rawValue
+    return input.rawValue
 }
